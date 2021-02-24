dd2d7be05cc2d64cb56e896991c74f5e8beaff28
Ren0503 <illuminati0440@gmail.com>
2021-02-24 10:19:01 +0700

    Connect new database

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://localhost:27017/chatapp",
+  mongoURI: "mongodb+srv://user0503:116105101110@cluster0.iavfm.mongodb.net/chatapp?retryWrites=true&w=majority",
   secretOrKey: "secret",
 };
